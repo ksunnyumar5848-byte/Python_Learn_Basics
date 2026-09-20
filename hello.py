632cc554db217d6095620840f62f700058c7f608
ksunnyumar5848 <ksunnyumar5848@gmail.com>
2026-09-20 08:23:15 +0530

    variable data type . user input f string

--- hello.py
+++ hello.py
@@ -1,1 +1,27 @@
 print("Practice the python concept ")
+
+
+#some python how to workd ans input and output and type casting 
+
+name="sunny kumar "
+age=20
+location="jharkhand"
+teacher="Vikash sir "
+profession="bca "
+
+print("=============all the information about the user ==============\nclea")
+print(f" hello {teacher} my name is  {name}\n i form {location} i persuring {profession} \n  i learn the python this language are very ")
+print(f"i have one team member and his also working profession he work on Accenture company and and many company \n ")
+
+
+#types casting 
+print("------------input information form  person ---------------------")
+
+myname=input("Enter the name of user ")
+myprofession=input("Enter the work profession of user ")
+company=input("Enter the company name ")
+
+
+print("print the user input information and this data type \n ")
+
+print(f"my name {myname} and data type--->{type(myname)} \n \n working profession {myprofession}\n  and i currenty woring in {company}")
